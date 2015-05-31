54bce2bd1db13c2e54baacf4ad1eb80ff56e58e0
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-05-31 14:33:48 +0100

    All races done

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,48 @@ $window.scroll(function() {
 		$("#navbar").removeClass("after-scroll");
 	}
 });
+
+function showAsura() {
+	document.getElementById('race-id').style.display="block";
+	document.getElementById('asura-id').style.display="block";
+	document.getElementById('sylvari-id').style.display="none";
+	document.getElementById('human-id').style.display="none";
+	document.getElementById('norn-id').style.display="none";
+	document.getElementById('charr-id').style.display="none";
+}
+
+function showSylvari() {
+	document.getElementById('race-id').style.display="block";
+	document.getElementById('asura-id').style.display="none";
+	document.getElementById('sylvari-id').style.display="block";
+	document.getElementById('human-id').style.display="none";
+	document.getElementById('norn-id').style.display="none";
+	document.getElementById('charr-id').style.display="none";
+}
+
+function showHuman() {
+	document.getElementById('race-id').style.display="block";
+	document.getElementById('asura-id').style.display="none";
+	document.getElementById('sylvari-id').style.display="none";
+	document.getElementById('human-id').style.display="block";
+	document.getElementById('norn-id').style.display="none";
+	document.getElementById('charr-id').style.display="none";
+}
+
+function showNorn() {
+	document.getElementById('race-id').style.display="block";
+	document.getElementById('asura-id').style.display="none";
+	document.getElementById('sylvari-id').style.display="none";
+	document.getElementById('human-id').style.display="none";
+	document.getElementById('norn-id').style.display="block";
+	document.getElementById('charr-id').style.display="none";
+}
+
+function showCharr() {
+	document.getElementById('race-id').style.display="block";
+	document.getElementById('asura-id').style.display="none";
+	document.getElementById('sylvari-id').style.display="none";
+	document.getElementById('human-id').style.display="none";
+	document.getElementById('norn-id').style.display="none";
+	document.getElementById('charr-id').style.display="block";
+}
