7fd2a429b31c98dab67eba81c05ccd24efaa9785
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-06-05 02:06:54 +0100

    Music implemented

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,10 +1,11 @@
+/*
 jQuery(function($) {
 	var supportsAudio = !!document.createElement('audio').canPlayType;
 	
 	if(supportsAudio) {
 		var index = 0,
 		playing = false;
-		mediaPath = '/music/',
+		mediaPath = 'music/',
 		extension = '',
 		tracks = [
 			{"track":1,"name":"Overture","length":"01:29","file":"Guild Wars 2 OST - 01. Overture"},
@@ -86,3 +87,45 @@ jQuery(function($) {
 		loadTrack(index);
 	}
 });
+*/
+
+$(document).ready(function () {
+	var audio;
+	var playlist;
+	var tracks;
+	var current;
+
+	init();
+	function init() {
+		current = 0;
+		audio = $('#audio');
+		playlist = $('#playlist');
+		tracks = playlist.find('li a');
+		len = tracks.length - 1;
+		audio[0].volume = 1;
+		audio[0].play();
+		playlist.find('a').click(function (e) {
+			e.preventDefault();
+			link = $(this);
+			current = link.parent().index();
+			run(link, audio[0]);
+		});
+		audio[0].addEventListener('ended', function (e) {
+			current++;
+			if (current == len) {
+				current = 0;
+				link = playlist.find('a')[0];
+			} else {
+				link = playlist.find('a')[current];
+			}
+			run($(link), audio[0]);
+		});
+	}
+	function run(link, player) {
+		player.src = link.attr('href');
+		par = link.parent();
+		par.addClass('active').siblings().removeClass('active');
+		audio[0].load();
+		audio[0].play();
+		}
+})
